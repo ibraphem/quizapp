81dc6c363de2db219362811867f1a12af58852f1
ibraphem <ibraphem@gmail.com>
2022-04-18 12:08:29 +0100

    Adjust Title

--- src/components/QuizResult.tsx
+++ src/components/QuizResult.tsx
@@ -12,7 +12,7 @@ const QuizResult: React.FC<QuizResultProps> = ({score, tryQuizAgain}) => {
         <Wrapper>
             <p className="score">Score: {score}</p>
             <div>
-                <img src={ThumbsUp} alt="thumbs up" style={{width: '100%', height:'250px'}}/>
+                <img src={ThumbsUp} alt="thumbs up" style={{width: '100%', height:'200px'}}/>
             </div>
             <button className="next" onClick={tryQuizAgain}>
                 Try Again
--- src/components/styled/App.Styles.ts
+++ src/components/styled/App.Styles.ts
@@ -48,8 +48,8 @@ export const Wrapper = styled.div`
         -moz-background-clip: text;
         -moz-text-fill-color: transparent;
         filter: drop-shadow(2px 2px #0085a3);
-        font-size: 70px;
-        font-weight: 400px
+         font-size: clamp(2.5rem, 5vw, 6rem);
+        font-weight: 400px;
         text-align: center;
         margin: 20px;
     }
@@ -57,7 +57,7 @@ export const Wrapper = styled.div`
     .start, .next {
         cursor: pointer;
         background-image: linear-gradient(180deg, #fff, #87F1ff);
-        border: 2px solid #d3855;
+        border: 2px solid #d38556;
         box-shadow: 0px, 5px 10px rgba(0, 0, 0, 0.25);
         border-radius: 10px;
         height: 40px;
